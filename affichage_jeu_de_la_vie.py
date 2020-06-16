3fe1288561b23a7a74d83faef0a7ef97a65c5935
CyprienTrn <66848658+CyprienTrn@users.noreply.github.com>
2020-06-16 09:49:15 +0200

    Update affichage_jeu_de_la_vie.py
    - réorganisation du code
    - ajout de plus de commentaires et de docstrings
    - affichage de la grille sans les bordures
    - on peut désormais cliquer uniquement sur la grille affichée dans le canvas
    - ajout d'un label pour suivre le nombre de générations en temps réel

--- affichage_jeu_de_la_vie.py
+++ affichage_jeu_de_la_vie.py
@@ -10,6 +10,11 @@ import sys
 project_folder = path.dirname(__file__)
 
 
+# ===============================================================
+# Fonctions relatives au calculs de coordonnées de la grille
+# ===============================================================
+
+
 def idx_to_coord(i, j):
     """Converti les coordonnées de la grille en coordonnées du canevas.
 
@@ -19,7 +24,7 @@ def idx_to_coord(i, j):
     :type j: int.
 
     :returns: Les coordonnées des deux points pour remplir le rectangle.
-    :rtype: 2 tuples.
+    :rtype: un tuple contenant deux tuples.
     """
     # on calcule d'abord les coordonnées des points
     x1 = grid_size * j - 1
@@ -43,34 +48,34 @@ def coord_to_idx(x, y):
     return (y // grid_size, x // grid_size)
 
 
-def import_prefabs():
-    """Permet d'importer une grille préfabriquée."""
-    global nlignes, ncolonnes, grille
-
-    # on lit le fichier qui contient la grille :
-    fichier = path.join(project_folder, "prefabs/canon.txt")
-    grille = jdlv.lire_fichier(fichier)
-    display_grid(grille)
+# ==============================================================================
+# Fonctions relatives à l'interaction entre l'interface et l'utilisateur
+# ==============================================================================
 
 
 def clic_canevas(event, click_side):
-    """Action effectué lors du clic sur le canvas (grille).
+    """Modifie l'affichage de la grille lors du clic sur le canvas.
+
+    :param click_side: le côté clické de la souris.
+    :type clicl_side: str.
     """
     global grille
     x, y = event.x, event.y
     i, j = coord_to_idx(x, y)
 
-    point1, point2 = idx_to_coord(i, j)
+    # si on clique dans la grille affichée (sans les bordures):
+    if jdlv.rab_bordure <= i < len(grille) - jdlv.rab_bordure and \
+        jdlv.rab_bordure <= j < len(grille[0]) - jdlv.rab_bordure:
 
-    if click_side == "left":
-        grille[i][j] = 1
-        couleur = "black"
-    else:
-        grille[i][j] = 0
-        couleur = "white"
+        if click_side == "left":
+            grille[i][j] = 1
+            couleur = "black"
+        else:
+            grille[i][j] = 0
+            couleur = "white"
 
-    canvas.itemconfig(cellules[j][i], fill=couleur)
-    jdlv.affiche(grille)
+        canvas.itemconfig(cellules[j][i], fill=couleur)
+        jdlv.affiche(grille)
 
 
 def display_grid(grid):
@@ -82,7 +87,6 @@ def display_grid(grid):
     # On l'affiche dans le canvas de Tkinter
     for l in range(jdlv.rab_bordure, nlignes - jdlv.rab_bordure):
         for c in range(jdlv.rab_bordure, ncolonnes - jdlv.rab_bordure):
-            point1, point2 = idx_to_coord(l, c)
 
             if grid[l][c] == 1:
                 couleur = "black"
@@ -94,27 +98,64 @@ def display_grid(grid):
 
 def clean_grid(event=None):
     """Nettoie la grille."""
-    global grille, play_flag
+    global grille, play_flag, n_generations
+
+    # on arrête l'exécution en boucle de la fonction en_boucle()
     play_flag = False
+
+    # on vide la grille et on l'affiche
     grille = jdlv.grille_vide(nlignes, ncolonnes)
     display_grid(grille)
+
+    # on réinitialise les nombre de génération à 0 et on actualise l'affichage
+    n_generations = 0
+    VarLabelGenerations.set(n_generations)
+
+
+# ================================================
+# Fonctions pour faire jouer la simulation
+# ================================================
 
 
 def en_boucle():
     """Permet de jouer en boucle la simulation du jeu de la vie."""
-    global grille
+    global grille, n_generations
 
     if play_flag:
+        # si on peut jouer :
+
         grille = jdlv.une_generation(grille)
         display_grid(grille)
+
+        # on ajoute une génération au label, et on l'actualise
+        n_generations += 1
+        VarLabelGenerations.set(n_generations)
+
+        # et on refait appel à cette fonction la fonction
         root.after(1, en_boucle)
 
 
 def play(event=None):
+    """Fonction qui fait se développer la grille en boucle."""
     global play_flag
     
     play_flag = True
     en_boucle()
+    
+
+# ================================================================
+# Bonus : importations de grilles, génération aléatoire, etc
+# ================================================================
+
+    
+def import_prefabs():
+    """Permet d'importer une grille préfabriquée."""
+    global nlignes, ncolonnes, grille
+
+    # on lit le fichier qui contient la grille :
+    fichier = path.join(project_folder, "prefabs/canon.txt")
+    grille = jdlv.lire_fichier(fichier)
+    display_grid(grille)
 
 
 def generation_aleatoire_de_grille():
@@ -130,6 +171,7 @@ def generation_aleatoire_de_grille():
     display_grid(grille)
     return grille
 
+
 nlignes = 14 + (jdlv.rab_bordure * 2)
 ncolonnes = 36 + (jdlv.rab_bordure * 2)
 grille = jdlv.grille_vide(nlignes, ncolonnes)
@@ -139,10 +181,14 @@ grid_size = 25
 cwidth = ncolonnes * grid_size
 cheight = nlignes * grid_size
 
-# variable qui permet d'arrêter la simulation
-play_flag = bool()
-
 if __name__ == "__main__":
+
+    # variable qui permet d'arrêter la simulation
+    play_flag = bool()
+
+    # variable qui nous permet de compter le nombre de générations
+    n_generations = int()
+
     # =================
     # FENETRE
     # =================
@@ -159,9 +205,24 @@ if __name__ == "__main__":
     canvas.bind("<Button-1>", lambda event: clic_canevas(event, "left"))
     canvas.bind("<Button-3>", lambda event: clic_canevas(event, "right"))
 
+    # on dessine la grille et on stocke les cases (pour les modifier plus tard)
+    cellules = [[canvas.create_rectangle(idx_to_coord(l, c), outline="gray") for l in range(nlignes)] for c in range(ncolonnes)]
+
+
     # on créer la frame principale de la fenêtre
     main_frame = Frame(root, bg="#404040")
     main_frame.pack(fill="both", expand=True)
+
+
+    # Création d'une variable de contrôle qui permet de modifier le text du label sans 
+    # avoir à passer par une fonction.
+    VarLabelGenerations = StringVar()
+    VarLabelGenerations.set(n_generations)
+
+    # un label pour afficher les victoires/matchs nuls
+    generation_label = Label(main_frame, textvariable=VarLabelGenerations, font=("Arial", 20), bg="#404040", fg="red")
+    generation_label.pack(expand=True)
+
 
     retrecicement_images = 5
     width_images = 512/retrecicement_images
@@ -194,9 +255,6 @@ if __name__ == "__main__":
     # on créer un bouton (à remplacer par un truc plus propre) pour sauvegarder la grille créée
     save_button = Button(main_frame, text='save', command=lambda: jdlv.sauvegarder_grille(grille))
     save_button.pack()
-
-    # on dessine la grille et on stocke les cases
-    cellules = [[canvas.create_rectangle(idx_to_coord(l, c), outline="gray") for l in range(nlignes)] for c in range(ncolonnes)]
 
     display_grid(grille)
 

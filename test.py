62776dd781cd2c07e75ba84284964c91ed934900
CyprienTrn <66848658+CyprienTrn@users.noreply.github.com>
2020-06-16 19:01:22 +0200

    Update test.py
    ajout de commentaires

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from os import path
 # on stocke l'endroit ou est notre programme.
 project_folder = path.dirname(__file__)
 
+# on créer un dictionnaire avec des le nom du fichier à tester en clef et le nombre
+# d'itérations à faire pour que le test soit celui prévu
 a_tester = {
     "regles_de_base.txt" : 1,
     "balise.txt" : 1,
@@ -21,9 +23,12 @@ a_tester = {
 }
 
 for structure, iterations in a_tester.items():
+    # on lit le fichier qui nous donne la structure finale
     structure_finale = lire_fichier(path.join(project_folder, "tests/grilles_finales/" + structure))
+    # et on lit le fichier qui nous donne la structure de base
     structure = lire_fichier(path.join(project_folder, "tests/grilles_test/" + structure))
 
+    # on fait le test
     assert lancer_jeu_console(iterations, structure, False) == structure_finale
 
 print("Tout s'est bien déroulé !")
